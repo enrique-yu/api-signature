f9a6ad10b29abc6c5413e6de84cdd49ebeb0c0cd
icoolkj <icoolkj@163.com>
2023-11-03 21:08:03 +0800

    去除对Hutool工具包的依赖

--- icoolkj-api-wrap-core/src/main/java/com/icoolkj/api/wrap/core/utils/AESUtils.java
+++ icoolkj-api-wrap-core/src/main/java/com/icoolkj/api/wrap/core/utils/AESUtils.java
@@ -1,9 +1,5 @@
 package com.icoolkj.api.wrap.core.utils;
 
-import cn.hutool.core.codec.Base64;
-import cn.hutool.log.Log;
-import cn.hutool.log.LogFactory;
-
 import javax.crypto.Cipher;
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
@@ -11,10 +7,13 @@ import javax.crypto.spec.SecretKeySpec;
 import java.nio.charset.StandardCharsets;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
+import java.util.Base64;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class AESUtils
 {
-    private static final Log log = LogFactory.get(AESUtils.class);
+    private static final Logger log = Logger.getLogger(AESUtils.class.getName());
     private static final String KEY_ALGORITHM = "AES";
     private static final String DEFAULT_CIPHER_ALGORITHM = "AES/ECB/PKCS5Padding"; //默认的加密算法
     private static final int DEFAULT_KEY_SIZE = 128; // 默认的密钥长度
@@ -39,7 +38,6 @@ public class AESUtils
      * @return 返回Base64转码的加密数据
      */
     public static String encrypt(String data, String password, int keySize) {
-        String encryptedData = null;
         try {
             // 实例化
             Cipher cipher = Cipher.getInstance(DEFAULT_CIPHER_ALGORITHM);
@@ -49,11 +47,12 @@ public class AESUtils
             // 加密
             byte[] encrypted = cipher.doFinal(byteData);
             // 通过Base64转码
-            encryptedData = Base64.encode(encrypted);
+            return Base64.getEncoder().encodeToString(encrypted);
         } catch (Exception ex) {
-            log.error(ex);
+            // ex.printStackTrace();
+            log.log(Level.SEVERE, "Error during encryption", ex);
+            return null;
         }
-        return  encryptedData;
     }
 
     /**
@@ -76,19 +75,19 @@ public class AESUtils
      * @return 返回解密后数据
      */
     public static String decrypt(String encryptedData, String password, int keySize) {
-        String decryptedData = null;
         try {
             // 实例化
             Cipher cipher = Cipher.getInstance(DEFAULT_CIPHER_ALGORITHM);
             // 初始化解密模式的密码器
             cipher.init(Cipher.DECRYPT_MODE, getSecretKey(password, keySize));
             // 执行操作
-            byte[] decrypted = cipher.doFinal(Base64.decode(encryptedData));
-            decryptedData = new String(decrypted, StandardCharsets.UTF_8);
+            byte[] decrypted = cipher.doFinal(Base64.getDecoder().decode(encryptedData));
+            return new String(decrypted, StandardCharsets.UTF_8);
         } catch (Exception ex) {
-            log.error(ex);
+            // ex.printStackTrace();
+            log.log(Level.SEVERE, "Error during decryption", ex);
+            return null;
         }
-        return decryptedData;
     }
 
     /**
@@ -100,7 +99,6 @@ public class AESUtils
      */
     private static SecretKeySpec getSecretKey(final String password, int keySize) {
         // 返回生成指定算法密钥 SecretKeySpec 对象
-        SecretKeySpec secretKeySpec = null;
         try {
             KeyGenerator kg = KeyGenerator.getInstance(KEY_ALGORITHM);
             // AES 要求密钥长度为 128, 192 或 256
@@ -108,10 +106,11 @@ public class AESUtils
             // 生成一个密钥
             SecretKey secretKey = kg.generateKey();
             // 转换为AES专用密钥
-            secretKeySpec = new SecretKeySpec(secretKey.getEncoded(), KEY_ALGORITHM);
+            return new SecretKeySpec(secretKey.getEncoded(), KEY_ALGORITHM);
         } catch (NoSuchAlgorithmException ex) {
-            log.error(ex);
+            // ex.printStackTrace();
+            log.log(Level.SEVERE, "Error during key generation", ex);
+            return null;
         }
-        return secretKeySpec;
     }
 }
